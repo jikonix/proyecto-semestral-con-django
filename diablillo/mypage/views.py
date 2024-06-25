43d16104b7d7bcc08e554e78e32baaea0d0b2f75
jikonix <127434705+jikonix@users.noreply.github.com>
2024-06-24 21:14:04 -0400

    traspasando a django omaiga

--- diablillo/mypage/views.py
+++ diablillo/mypage/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader
+from django.db import models
+from django.views import view
+
 
 # Create your views here.
+class homepage(view):
+    template= 
