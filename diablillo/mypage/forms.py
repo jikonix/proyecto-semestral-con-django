1f0216513afb17cd9864e0f71f4b41b3e378bc73
jikonix <127434705+jikonix@users.noreply.github.com>
2024-07-04 23:09:35 -0400

    re armando el projecto final

--- diablillo/mypage/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Pizza
-
-class nueva_pizza(forms.ModelForm):
-    class Meta:
-        model=  Pizza
-        fields= "__all__"
--- diablillo/mypage/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Pizza(models.Model):
-    nombre_pizza = models.CharField(max_length= 20, unique=True, null=False)
-    precio= models.IntegerField(null=False)
-
-    def __str__(self) -> str:
-        return f"{self.nombre_pizza}, {self.precio}"
